c78b78b48ebaf89732615c6c9748b4856abd1d43
ViraaShop001 <ranggaraditiawan9@gmail.com>
2026-02-26 15:05:14 +0700

    Refactor file fetching logic and update constants

--- api/file/[name].js
+++ api/file/[name].js
@@ -1,19 +1,17 @@
-const USERNAME = "ViraaShop001" // ganti
-const REPO = "uploader"
+const TOKEN = "dm0SIK2U+I0f6Bx8h1b2DaE3IHyCarCN2H2lncDjmwrJTs/zeU30yklisRyV1PfJ";
+const USERNAME = "ViraaShop001";
+const REPO = "uploader";
+const BRANCH = "main";
 
-export default async function handler(req,res){
-  const { name } = req.query
+export default async function handler(req, res) {
+  const { name } = req.query;
 
-  const gh = await fetch(
-    `https://raw.githubusercontent.com/${USERNAME}/${REPO}/main/uploads/${name}`
-  )
+  const r = await fetch(
+    `https://raw.githubusercontent.com/${USERNAME}/${REPO}/${BRANCH}/cdn/${name}`
+  );
 
-  if(!gh.ok)
-    return res.status(404).send("File not found")
+  if (!r.ok) return res.status(404).send("Not found");
 
-  res.setHeader("Cache-Control","public, max-age=31536000, immutable")
-  res.setHeader("Content-Type", gh.headers.get("content-type"))
-
-  const buffer = await gh.arrayBuffer()
-  res.send(Buffer.from(buffer))
-}
+  const buffer = await r.arrayBuffer();
+  res.send(Buffer.from(buffer));
+                                         }
